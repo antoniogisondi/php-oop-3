0141364d8f31de4798d9b0d9ac9e12c3ba30cfee
antoniogisondi <anto.gisondi@tiscali.it>
2023-07-13 14:43:40 +0200

    Exercise done

--- db.php
+++ db.php
@@ -1,67 +1,28 @@
 <?php 
 // STAMPO A SCHERMO LE ISTANZE DELLA CLASSE COMUNICAZIONE
-$mess = new Comunicazione('Antonio', 'Giuseppe', 'Titolo della comunicazione','Contenuto della comunicazione');
-echo "Mittente: ".$mess->getMittente()."<br/>";
-echo "Destinatario: ".$mess->getDestinatario()."<br/>";
-echo "Titolo: ".$mess->getTitolo()."<br/>";
-echo "Messaggio: ".$mess->getMessaggio()."<br/>";
-echo "Stato invio: ".$mess->invio()."<br/>";
+$comunications = [
+    new Comunicazione('Antonio', 'Giuseppe', 'Titolo della comunicazione','Contenuto della comunicazione'),
+];
 
-echo "<hr/>";
+$emails = [
+    new Email('Antonio', 'Andrea', 'Titolo della mail', 'Contenuto della mail', 'allegato.pdf', true),
+    new Email('Antonio', 'Ferruccio', 'Titolo della mail', 'Contenuto della mail', 'allegato.word', false),
+    new Email('Antonio', 'Gianmauro', 'Titolo della mail', 'Contenuto della mail', 'allegato.png', true),
+    new Email('Antonio', 'Daniele', 'Titolo della mail', 'Contenuto della mail', 'allegato.jpg', false),
+];
 
-// STAMPO A SCHERMO LE ISTANZE DELLA CLASSE EMAIL
-$email = new Email('Antonio', 'Giuseppe', 'Titolo della mail', 'Contenuto della mail', 'allegato.pdf', true);
-echo "Mittente: ".$email->getMittente()."<br/>";
-echo "Destinatario: ".$email->getDestinatario()."<br/>";
-echo "Titolo: ".$email->getTitolo()."<br/>";
-echo "Messaggio: ".$email->getMessaggio()."<br/>";
-echo "Allegato: ".$email->getAllegato()."<br/>";
-if($email->getNotificaConsegna()){
-    echo 'L\'email è stata consegnata correttamente <br/>';
-}
-else{
-    echo 'L\'email non è stata consegnata correttamente <br/>';
-}
-echo "Stato invio: ".$email->invio()."<br/>";
-echo "Stato inoltro: ".$email->inoltro()."<br/>";
-echo "Stato stampa: ".$email->stampa()."<br/>";
-
-echo "<hr/>";
-
-// STAMPO A SCHERMO LE ISTANZE DELLA CLASSE SMS
-$sms = new SMS('Antonio', 'Francesco', 'Titolo dell\' SMS', 'Contenuto dell\' SMS', true, false);
-echo "Da ".$sms->getMittente()."<br/>";
-echo "a ".$sms->getDestinatario()."<br/>";
-echo $sms->getTitolo()."<br/>";
-echo "Messaggio: ".$sms->getMessaggio()."<br/>";
-echo $sms->invio()."<br/>";
-if($sms->getNotificaLettura()){
-    echo 'Il messaggio è stato visualizzato <br/>';
-}
-else{
-    echo 'Il messaggio non è stato visualizzato <br/>';
-}
-
-if($sms->getRispostaAccettata()){
-    echo 'Hai ricevuto risposta <br/>';
-}
-else{
-    echo 'Non hai ricevuto risposta <br/>';
-}
-
-echo "<hr/>";
-
-$notify = new Notifiche_PUSH('Antonio', 'Instagram', 'Titolo della notifica', 'Contenuto della notifica', false, 'Icona della notifica');
-echo $notify->getMittente()." ";
-echo "hai ricevuto una notifica da ".$notify->getDestinatario()."<br/>";
-echo $notify->getTitolo()."<br/>";
-echo $notify->getMessaggio()."<br/>";
-if($notify->getEventoClick()){
-    echo 'La notifica è stata letta <br/>';
-}
-else{
-    echo 'Hai 1 notifica non letta <br/>';
-}
-echo $notify->getIcona();
-
+$sms = [
+    new SMS('Antonio', 'Francesco', 'Titolo dell\' SMS', 'Contenuto dell\' SMS', true, true),
+    new SMS('Antonio', 'Michele', 'Titolo dell\' SMS', 'Contenuto dell\' SMS', true, false),
+    new SMS('Antonio', 'Giovanni', 'Titolo dell\' SMS', 'Contenuto dell\' SMS', false, false),
+];
+ 
+$notifies = [
+    new Notifiche_PUSH('Antonio', 'Instagram', 'Titolo della notifica', 'Contenuto della notifica', false, 'Icona della notifica'),
+    new Notifiche_PUSH('Antonio', 'Whatsapp', 'Titolo della notifica', 'Contenuto della notifica', true, 'Icona della notifica'),
+    new Notifiche_PUSH('Antonio', 'TikTok', 'Titolo della notifica', 'Contenuto della notifica', false, 'Icona della notifica'),
+    new Notifiche_PUSH('Antonio', 'Linkedin', 'Titolo della notifica', 'Contenuto della notifica', false, 'Icona della notifica'),
+    new Notifiche_PUSH('Antonio', 'Facebook', 'Titolo della notifica', 'Contenuto della notifica', true, 'Icona della notifica'),
+    new Notifiche_PUSH('Antonio', 'Snapchat', 'Titolo della notifica', 'Contenuto della notifica', false, 'Icona della notifica'),
+];  
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php 
-    require_once __DIR__.'/models/allegato.php';
     require_once __DIR__.'/models/comunication.php';
     require_once __DIR__.'/models/email.php';
     require_once __DIR__.'/models/notifications.php';
@@ -16,11 +15,159 @@
         <title>Php OOP 3</title>
     </head>
     <body>
-        <header>
-
+        <header class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <h1>Comunication System</h1>
+                </div>
+            </div>
         </header>
-        <main>
+        <main class="container-fluid">
+            <div class="row d-flex flex-column">
+                <div class="col mt-2">
+                <h3>Comunicazione</h3>
+                    <div class="card">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Mittente</th>
+                                    <th scope="col">Destinatario</th>
+                                    <th scope="col">Oggetto</th>
+                                    <th scope="col">Messaggio</th>
+                                    <th scope="col">Notifica</th>
+                                </tr>
+                            </thead>
 
+                            <tbody>
+                                <?php foreach($comunications as $comm) { ?>
+                                    <tr>
+                                        <td><?php echo $comm->getMittente() ?></td>
+                                        <td><?php echo $comm->getDestinatario() ?></td>
+                                        <td><?php echo $comm->getTitolo() ?></td>
+                                        <td><?php echo $comm->getMessaggio() ?></td>
+                                        <td><?php echo $comm::$notification ?></td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div class="col mt-4">
+                    <h3>Email</h3>
+                    <div class="card">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Mittente</th>
+                                    <th scope="col">Destinatario</th>
+                                    <th scope="col">Oggetto</th>
+                                    <th scope="col">Messaggio</th>
+                                    <th scope="col">Allegato</th>
+                                    <th scope="col">Notifica di consegna</th>
+                                    <th scope="col">Colore LED</th>
+                                    <th scope="col">Notifica</th>
+                                    <th scope="col">Invio/Inoltro/Stampa</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <?php foreach($emails as $email) { ?>
+                                    <tr>
+                                        <td><?php echo $email->getMittente() ?></td>
+                                        <td><?php echo $email->getDestinatario() ?></td>
+                                        <td><?php echo $email->getTitolo() ?></td>
+                                        <td><?php echo $email->getMessaggio() ?></td>
+                                        <td><?php echo $email->allegato ?></td>
+                                        <td><?php echo $email->getNotificaConsegna() ? 'L\'email è stata consegnata' : 'L\'email non è stata consegnata' ?></td>
+                                        <td><?php echo $email::$ledColor ?></td>
+                                        <td><?php echo $email::$notification ?></td>
+                                        <td>
+                                            <div class="d-flex">
+                                                <div class="mx-2"><?php echo $email->invio() ?></div>
+                                                <div class="mx-2"><?php echo $email->inoltro()?></div>
+                                                <div class="mx-2"><?php echo $email->stampa() ?></div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div class="col mt-4">
+                    <h3>SMS</h3>
+                    <div class="card">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Mittente</th>
+                                    <th scope="col">Destinatario</th>
+                                    <th scope="col">Oggetto</th>
+                                    <th scope="col">Messaggio</th>
+                                    <th scope="col">Invio del messaggio</th>
+                                    <th scope="col">Notifica di lettura</th>
+                                    <th scope="col">Notifica di risposta</th>
+                                    <th scope="col">Colore LED</th>
+                                    <th scope="col">Invio</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <?php foreach($sms as $ssm) { ?>
+                                    <tr>
+                                        <td><?php echo $ssm->getMittente() ?></td>
+                                        <td><?php echo $ssm->getDestinatario() ?></td>
+                                        <td><?php echo $ssm->getTitolo() ?></td>
+                                        <td><?php echo $ssm->getMessaggio() ?></td>
+                                        <td><?php echo $ssm->invio() ?></td>
+                                        <td><?php echo $ssm->getNotificaLettura() ? 'Il messaggio è stato letto' : 'Il messaggio non è stato letto' ?></td>
+                                        <td><?php echo $ssm->getRispostaAccettata() ? 'Sta scrivendo...' : 'Non hanno risposto al tuo messaggio' ?></td>
+                                        <td><?php echo $ssm::$ledColor ?></td>
+                                        <td><?php echo $ssm::$notification ?></td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div class="col mt-4">
+                    <h3>Notifiche PUSH</h3>
+                    <div class="card">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Mittente</th>
+                                    <th scope="col">Destinatario</th>
+                                    <th scope="col">Oggetto della notifica</th>
+                                    <th scope="col">Contenuto della notifica</th>
+                                    <th scope="col">Notifica visualizzata</th>
+                                    <th scope="col">Icona</th>
+                                    <th scope="col">Colore LED</th>
+                                    <th scope="col">Notifica</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <?php foreach($notifies as $notify) { ?>
+                                    <tr>
+                                        <td><?php echo $notify->getMittente() ?></td>
+                                        <td><?php echo $notify->getDestinatario() ?></td>
+                                        <td><?php echo $notify->getTitolo() ?></td>
+                                        <td><?php echo $notify->getMessaggio() ?></td>
+                                        <td><?php echo $notify->getEventoClick() ? 'La notifica è stata visualizzata' : 'Hai 1 notifica non letta'?></td>
+                                        <td><?php echo $notify->getIcona() ?></td>
+                                        <td><?php echo $notify::$ledColor ?></td>
+                                        <td><?php echo $notify::$notification ?></td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </main>
     </body>
 </html>
